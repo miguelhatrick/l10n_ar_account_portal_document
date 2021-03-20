cd2fbf46b0c08e7bdd3d82a16da51610bfc14339
Miguel <miguelhatrick@gmail.com>
2021-03-19 22:21:12 -0300

    Update res_custom_partner.py

--- models/res_custom_partner.py
+++ models/res_custom_partner.py
@@ -18,25 +18,21 @@ class ResCustomPartner(models.Model):
     def create(self, vals):
         """Custom function to copy the vat into l8n_ar document_id"""
 
-
-
-
-
         result = super(ResCustomPartner, self).create(vals)
         return result
 
     @api.multi
-    def write(self, vals):
+    def write(self, values):
         _logger.info('if vat is available, set the DNI data as a copy of it ... ')
         # if vat is available, set the DNI data as a copy of it
-        if 'vat' in vals and vals['vat'] and not self.main_id_number:
+        if 'vat' in values and values['vat'] and not self.main_id_number:
             _logger.info('Copying VAT ID to Document_ID ... ')
 
-            vals['main_id_number'] = vals['vat']
-            vals['main_id_category_id'] = 35
-            vals['afip_responsability_type_id'] = 6
+            values['main_id_number'] = values['vat']
+            values['main_id_category_id'] = 35
+            values['afip_responsability_type_id'] = 6
 
-        result = super(ResCustomPartner, self).write(vals)
+        result = super(ResCustomPartner, self).write(values)
         # Update
 
         return result
@@ -45,12 +41,12 @@ class ResCustomPartner(models.Model):
 
     # select id, vat, main_id_number, main_id_category_id,afip_responsability_type_id from res_partner where name like '%hatrick%'
 
-    # _event = self.env['event.event'].search([('id', '=', vals['event_id'])])[0]
+    # _event = self.env['event.event'].search([('id', '=', values['event_id'])])[0]
     # _map = False
     #
     # # if event has a generator_id we are interested in this
     # if len(_event.event_generator_id):
-    #     _member = self.env['res.partner'].search([('id', '=', vals['partner_id'])])[0]
+    #     _member = self.env['res.partner'].search([('id', '=', values['partner_id'])])[0]
     #     _location = _event.address_id
     #
     #     if not len(_member) or not len(_location):
@@ -62,4 +58,4 @@ class ResCustomPartner(models.Model):
     #         raise ValidationError('The member package has no more credits / active packets (EventRegistration)')
     #
     #     if _map.calculate_remaining_credits():
-    #         vals['member_access_package_id'] = _map.id
+    #         values['member_access_package_id'] = _map.id
